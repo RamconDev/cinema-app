ea3618ea999f5e9f9fb636b0ab9ac915fc3cba87
RamconDev <hd00rc@gmail.com>
2025-12-01 20:57:18 -0400

    first configuration project

---
+++ app/__init__.py
@@ -0,0 +1,29 @@
+from flask import Flask
+
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+from config import DevelopmentConfig as Config # to development config
+# from config import TestingConfig as Config # to testing config
+# from config import Config # to production or default config
+
+db = SQLAlchemy()
+migrate = Migrate()
+
+def create_app(config_class=Config):
+    app = Flask(__name__)
+
+    app.config.from_object(config_class)
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    @app.route('/')
+    def index():
+        return "Hola mundo"
+
+    # Register blueprints
+    # from app.register_blueprints import my_bp
+    # app.register_blueprint(my_bp)
+
+    return app
---
+++ app/register_blueprints.py
@@ -0,0 +1,4 @@
+def register_blueprints(app):
+    pass
+    # from app.my_bp import routes
+    # my_bp = Blueprint('my_bp', __name__, template_folder='templates')
